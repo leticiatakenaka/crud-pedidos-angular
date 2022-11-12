617a5aeb0a657ed5933413dfeec33d94f969b3eb
leticiatakenaka <leticiatakenaka12@gmail.com>
2022-11-12 20:10:09 -0300

    primeiro commit

--- src/app/pedido-list/pedido-list.component.ts
+++ src/app/pedido-list/pedido-list.component.ts
@@ -11,6 +11,11 @@ export class PedidoListComponent implements OnInit {
   constructor(private pedidoService: PedidosService) {}
 
   public pedidos: Pedido[] = [];
+  public pedidosResolvidos: Pedido[] = [];
+
+  public isEdit: string = 'ADICIONAR PEDIDO';
+  public isShowAlert: boolean = false;
+  public alertText: string = '';
 
   novoPedido: Pedido = {
     pedidoNo: this.pedidoService.pedidos.length + 1,
@@ -24,17 +29,66 @@ export class PedidoListComponent implements OnInit {
     this.getPedidos();
   }
 
-  onCloseForm() {
-    //this.showFormPedido = false;
-    this.getPedidos();
-  }
-
   private getPedidos() {
     this.pedidos = this.pedidoService.getPedidosPendentes();
   }
 
-  //Botoes
+  private limpaCampos() {
+    this.novoPedido.descricao = '';
+    this.novoPedido.prioridade = 'Baixa';
+    this.novoPedido.tipo = 'Funcionalidade';
+    this.novoPedido.titulo = '';
+  }
+
+  //Add pedido
   salvarPedido() {
-    this.pedidos.push(this.novoPedido);
+    //EDITA PEDIDO
+    if (this.isEdit == 'EDITAR PEDIDO') {
+      this.pedidoService.pedidos[
+        this.pedidoService.pedidos.findIndex(
+          (e) => e.pedidoNo == this.novoPedido.pedidoNo
+        )
+      ] = { ...this.novoPedido };
+      this.novoPedido.pedidoNo = this.novoPedido.pedidoNo += 1;
+      this.alertText = 'editado';
+      this.showAlert();
+    } else {
+      //SALVA NOVO PEDIDO
+      this.pedidoService.pedidos.push({ ...this.novoPedido });
+
+      this.novoPedido.pedidoNo += 1;
+      this.alertText = 'adicionado';
+      this.showAlert();
+    }
+    this.isEdit = 'ADICIONAR PEDIDO';
+    this.getPedidos();
+    this.limpaCampos();
+  }
+
+  //Excluir
+  excluiPedido(id: number) {
+    this.pedidoService.pedidos.splice(
+      this.pedidoService.pedidos.findIndex((e) => e.pedidoNo == id),
+      1
+    );
+    this.alertText = 'excluido';
+    this.showAlert();
+    this.getPedidos();
+  }
+
+  editarPedido(pedido: Pedido) {
+    this.isEdit = 'EDITAR PEDIDO';
+    this.novoPedido.pedidoNo = pedido.pedidoNo;
+    this.novoPedido.descricao = pedido.descricao;
+    this.novoPedido.prioridade = pedido.prioridade;
+    this.novoPedido.tipo = pedido.tipo;
+    this.novoPedido.titulo = pedido.titulo;
+  }
+
+  showAlert(): void {
+    this.isShowAlert = true;
+    setTimeout(() => {
+      this.isShowAlert = false;
+    }, 1500);
   }
 }
--- src/app/pedidos.service.ts
+++ src/app/pedidos.service.ts
@@ -6,50 +6,8 @@ import { Pedido } from './pedido';
 })
 export class PedidosService {
   constructor() {}
-  // private pedidos: Pedido[] = [];
 
-  pedidos: Pedido[] = [
-    {
-      pedidoNo: 1,
-      titulo: 'Big Mac',
-      descricao:
-        'A sanduíche dupla mais cobiçada no mundo inteiro. Feita com dois suculentos hambúrgueres 100% carne de vaca, queijo fundido, pepino, cebola, alface e um molho irresistível. Uma combinação única.',
-      prioridade: 'Alta',
-      tipo: 'Funcionalidade',
-    },
-    {
-      pedidoNo: 2,
-      titulo: 'Quarterão com Queijo',
-      descricao:
-        'Um hambúrguer (100% carne bovina), queijo sabor cheddar, picles, cebola, ketchup, mostarda e pão com gergelim.',
-      prioridade: 'Alta',
-      tipo: 'Funcionalidade',
-    },
-    {
-      pedidoNo: 3,
-      titulo: 'Cheddar McMelt',
-      descricao:
-        'Um hambúrguer (100% carne bovina), molho sabor cheddar, cebola ao molho shoyu e pão escuro com gergelim.',
-      prioridade: 'Alta',
-      tipo: 'Funcionalidade',
-    },
-    {
-      pedidoNo: 4,
-      titulo: 'McNífico Bacon',
-      descricao:
-        'Um hambúrguer (100% carne bovina), bacon, alface americana, cebola, queijo sabor cheddar, tomate, maionese, ketchup, mostarda e pão com gergelim.',
-      prioridade: 'Alta',
-      tipo: 'Funcionalidade',
-    },
-    {
-      pedidoNo: 5,
-      titulo: 'Duplo Quarterão',
-      descricao:
-        'Dois hambúrgueres (100% carne bovina), mostarda, ketchup, cebola, queijo sabor cheddar e pão com gergelim.',
-      prioridade: 'Alta',
-      tipo: 'Funcionalidade',
-    },
-  ];
+  pedidos: Pedido[] = [];
 
   getPedidosPendentes(): Pedido[] {
     return this.pedidos.filter((pedido) => !pedido.resolvido);
